40fbccefac12a242ca3f5e3d6467faec64c374a4
yamada <a>
2017-09-02 18:10:57 +0900

    titleフェードイン

--- Assets/Script/Title/Title.cs
+++ Assets/Script/Title/Title.cs
@@ -1,21 +1,34 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Title : MonoBehaviour {
-	public GameObject touch_to_start;
-	private const int AVTIVE_CHANGE_COUNT = 50;
-	private int _active_count;
+	public Image touch_to_start;
+	private float _alpha_speed = 0.01f;
 	// Use this for initialization
 	void Start ( ) {
-		_active_count = 0;
+		touch_to_start.color = new Color( 1, 1, 1, 0 );
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		_active_count++;
-		if ( _active_count % AVTIVE_CHANGE_COUNT == 0 ) {
-			touch_to_start.SetActive( !touch_to_start.activeSelf );
+		float alpha = touch_to_start.color.a;
+		Debug.Log( alpha );
+		alpha += _alpha_speed;
+		if ( _alpha_speed > 0 ) {
+			if ( alpha > 1 ) {
+				alpha = 1;
+				_alpha_speed *= -1;
+			}
+		} else {
+			if ( alpha < 0 ) {
+				alpha = 0;
+				_alpha_speed *= -1;
+			}
 		}
+		touch_to_start.color = new Color( 1, 1, 1, alpha );
+
+		
 		if ( Device.getTouchPhase( ) == Device.PHASE.ENDED ) {
 			SceneManager.LoadScene( "ChapterSelect" );
 		}
